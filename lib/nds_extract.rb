defd8370647b5bbaaaac02e7e426f17798a2b290
Ramesh Chuwan <chhetrirahul2015@gmail.com>
2020-01-18 21:08:09 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -13,5 +13,10 @@ end
 # Find a way to accumulate the :worldwide_grosses and return that Integer
 # using director_data as input
 def gross_for_director(director_data)
-  
+  i = 0 
+  while i < director_data.length do
+    total = 0 
+    j = 0 
+    while 
+  end
 end
